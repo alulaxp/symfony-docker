a2e05ff5350b57f2bdd960857dfbc1e393776076
alulex <aleejandrast@gmail.com>
2021-05-14 17:14:55 +0200

    installed and implemented Logger

--- src/Controller/LibraryController.php
+++ src/Controller/LibraryController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller ;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LibraryController extends AbstractController 
 {
+
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+
+    }
+
 
     /**
      * 
      * @Route("library/list", name="library_list")
      */
 
-     public function list() {
+     public function list(Request $request) {
+
+        $title = $request->get('title', 'default');
+        $this->logger->info('List action called');
          $response = new JsonResponse();
          $response->setData([
              'success' => true,
@@ -26,6 +40,11 @@ class LibraryController extends AbstractController
                 [
                 'id' => 1,
                 'title' => 'Nocturno de Chile'
+                ],
+                [
+                'id' => 3,
+                'title' => $title
+
                     ]
             ]
          ]);
